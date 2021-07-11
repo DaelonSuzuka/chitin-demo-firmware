24907d55739accaa6353c9ae91a4ea62f72371b5
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-07-11 06:54:54 -0400

    Rewrote lcd shell command

--- src/lcd.c
+++ src/lcd.c
@@ -1,11 +1,35 @@
 #include "os/serial_port.h"
+#include "os/shell/shell.h"
 #include "os/shell/shell_command_utils.h"
+#include "os/shell/shell_keys.h"
+#include "os/shell/shell_utils.h"
 #include "os/system_time.h"
 #include "peripherals/timer.h"
 #include "pins.h"
 #include "rgb.h"
 
 /* ************************************************************************** */
+
+#define LCD_ROWS 4
+#define LCD_COLS 20
+
+struct {
+    uint8_t cursor_x;
+    uint8_t cursor_y;
+    char grid[LCD_ROWS][LCD_COLS];
+} lcd_state;
+
+/* ************************************************************************** */
+
+// EMPTY_UART_INTERFACE(uart);
+
+// void lcd_tx_char(char c) {
+//     uart.tx_char(c); //
+// }
+
+// void lcd_tx_string(const char *string) {
+//     uart.tx_string(string, '\0'); //
+// }
 
 void lcd_tx_char(char c) {
     uint8_t i;
@@ -108,50 +132,149 @@ void lcd_init(void) {
     // create_uart_buffers(lcd, config, 128);
 
     // uart = UART_init(&config);
+
+    memset(lcd_state.grid, ' ', 80);
+
     set_LCD_TX_PIN(1);
     timer2_clock_source(TMR2_CLK_FOSC4);
     timer2_postscale(TMR_POSTSCALE_10);
     timer2_period_set(168);
     timer2_start();
 
+    lcd_clear();
+
     shell_register_command(sh_lcd, "lcd");
 }
 
 /* ************************************************************************** */
 
-void sh_lcd(int argc, char **argv) {
-    switch (argc) {
-    case 1:
-        println("usage: \tlcd");
-        println("\tlcd clear");
-        println("\tlcd test");
-        println("\tlcd blink");
-        println("\tlcd write <text>");
-        println("\tlcd backlight <brightness>");
-        return;
-    case 2:
-        if (!strcmp(argv[1], "clear")) {
-            lcd_clear();
-            return;
-        } else if (!strcmp(argv[1], "test")) {
-            // uart.tx_string("beep", '\0');
-            lcd_tx_char('b');
-            lcd_tx_char('e');
-            lcd_tx_char('e');
-            lcd_tx_char('p');
-            return;
-        } else if (!strcmp(argv[1], "blink")) {
-            lcd_tx_char(CONTROL_1);
-            lcd_tx_char(CURSOR_BLINK);
-            return;
+void draw_lcd(void) {
+    term_reset_screen();
+    term_cursor_set(0, 0);
+
+    lcd_state.cursor_x = 0;
+    lcd_state.cursor_y = 0;
+
+    println("");
+    println("|  LCD Render Preview  |");
+    println("| -------------------- |");
+
+    for (uint8_t row = 0; row < LCD_ROWS; row++) {
+        print("| ");
+        for (uint8_t col = 0; col < LCD_COLS; col++) {
+            printf("%c", lcd_state.grid[row][col]);
         }
-        break;
-    case 3:
-        if (!strcmp(argv[1], "backlight")) {
-            lcd_set_backlight(atoi(argv[2]));
-            return;
-        }
-        break;
+        println(" |");
     }
-    println("invalid arguments");
+
+    println("| -------------------- |");
+    println("");
+
+    println("Anything you type in the above grid");
+    println("should appear on the LCD.");
+
+    println("");
+    println("Press \033[1;32mBACKSPACE\033[0;37;40m to clear the LCD");
+    println("Press \033[1;31mESCAPE\033[0;37;40m or \033[1;31mCTRL+C\033[0;37;40m to exit");
+
+    term_cursor_set(3, 4);
+}
+
+void lcd_cursor_up(void) {
+    if (lcd_state.cursor_y) {
+        term_cursor_up(1);
+        lcd_state.cursor_y--;
+        lcd_move_cursor((lcd_state.cursor_y * LCD_COLS) + lcd_state.cursor_x);
+    }
+}
+
+void lcd_cursor_down(void) {
+    if (lcd_state.cursor_y < LCD_ROWS - 1) {
+        term_cursor_down(1);
+        lcd_state.cursor_y++;
+        lcd_move_cursor((lcd_state.cursor_y * LCD_COLS) + lcd_state.cursor_x);
+    }
+}
+
+void lcd_cursor_left(void) {
+    if (lcd_state.cursor_x) {
+        term_cursor_left(1);
+        lcd_state.cursor_x--;
+        lcd_move_cursor((lcd_state.cursor_y * LCD_COLS) + lcd_state.cursor_x);
+    }
+}
+
+void lcd_cursor_right(void) {
+    if (lcd_state.cursor_x < LCD_COLS) {
+        term_cursor_right(1);
+        lcd_state.cursor_x++;
+        lcd_move_cursor((lcd_state.cursor_y * LCD_COLS) + lcd_state.cursor_x);
+    }
+}
+
+void lcd_insert_char(char currentChar) {
+    if (lcd_state.cursor_x < LCD_COLS) {
+        lcd_tx_char(currentChar);
+        printf("%c", currentChar);
+        lcd_state.grid[lcd_state.cursor_y][lcd_state.cursor_x] = currentChar;
+        lcd_state.cursor_x++;
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+
+int8_t sh_lcd_keys(key_t key) {
+    switch (key.key) {
+    default:
+        return 0;
+    case BACKSPACE:
+        lcd_state.cursor_x = 0;
+        lcd_state.cursor_y = 0;
+        memset(lcd_state.grid, ' ', 80);
+        draw_lcd();
+        lcd_clear();
+        return 0;
+    case DELETE:
+        return 0;
+    case UP:
+        lcd_cursor_up();
+        return 0;
+    case DOWN:
+        lcd_cursor_down();
+        return 0;
+    case LEFT:
+        lcd_cursor_left();
+        return 0;
+    case RIGHT:
+        lcd_cursor_right();
+        return 0;
+    case ENTER:
+        return 0;
+    case ESCAPE:
+        return -1;
+    }
+}
+
+/* ************************************************************************** */
+
+// loop - executed on every key press
+// must have a return to indicate desire to terminate program
+// if the correct state to do that comes about during the last keypress
+int8_t sh_lcd_callback(char currentChar) {
+    if (iscntrl(currentChar)) {
+        return sh_lcd_keys(identify_key(currentChar));
+    }
+
+    if (isprint(currentChar)) {
+        lcd_insert_char(currentChar);
+    }
+
+    return 0;
+}
+
+// setup
+void sh_lcd(int argc, char **argv) {
+    draw_lcd();
+
+    shell_register_callback(sh_lcd_callback);
 }
